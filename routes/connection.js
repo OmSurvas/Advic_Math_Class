0bcb1f08ecc66c1e768563a4aaf0ff4ddcfedfe4
OmSurvas <omsurvase86@gmail.com>
2024-12-03 15:05:40 +0530

    Update connection.js

--- routes/connection.js
+++ routes/connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var util = require("util");
 
 var conn = mysql.createConnection({
-  host:"localhost",
-  user :"root",
-  password:"",
-  database:"advik_genius_class"
+  host:"bcw3lvve5hvpwfysuhvt-mysql.services.clever-cloud.com",
+  user :"u0i6wyo4qazp1yr9",
+  password:"K3atq0KumP0iinjTgeQE",
+  database:"bcw3lvve5hvpwfysuhvt"
 })
 
 var exe = util.promisify(conn.query).bind(conn);
